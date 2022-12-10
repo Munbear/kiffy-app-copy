156cff88bd35b4606789a431f9b165c44804f31b
labyu <labyu2020@gmail.com>
2022-12-10 21:03:40 +0900

    프로필 등록 단순화

--- lib/view/add_profile/screen/setting_profile_screen.dart
+++ lib/view/add_profile/screen/setting_profile_screen.dart
@@ -10,51 +10,60 @@ import '../widget/selected_photos.dart';
 import '../widget/setting_birth.dart';
 import '../widget/setting_nick_name.dart';
 
+enum ProfileEditProcess {
+  Name,
+  Birthday,
+  Gender;
+}
+
+class ProfileEditProcessContent {
+  late String title;
+  late String guideText;
+
+  ProfileEditProcessContent({required this.title, required this.guideText});
+
+  factory ProfileEditProcessContent.of(ProfileEditProcess process) {
+    switch (process) {
+      case ProfileEditProcess.Name:
+        return ProfileEditProcessContent(title: "Name", guideText: "이름을 입력해주세요");
+      case ProfileEditProcess.Gender:
+        return ProfileEditProcessContent(title: "Gender", guideText: "성별을 입력해주세요");
+      case ProfileEditProcess.Birthday:
+        return ProfileEditProcessContent(title: "Birthday", guideText: "생일을 입력해주세요");
+    }
+  }
+}
+
 class SettingProfileScreen extends HookConsumerWidget {
   static String get routeName => 'setting';
+
   static String get routeLocation => '/setting';
 
   const SettingProfileScreen({super.key});
 
-  void upDateGuideText(ValueNotifier currentNumber, ValueNotifier title, ValueNotifier guideText) {
-    //이름
-    if (currentNumber.value == 0) title.value = "name";
-
-    // 생년월일
-    if (currentNumber.value == 1) title.value = "ultah";
-    if (currentNumber.value == 1) guideText.value = "Masukkan tanggal lahir Anda.";
-
-    // 성별
-    if (currentNumber.value == 2) title.value = "jenis kelamin";
-    if (currentNumber.value == 2) guideText.value = "Silakan pilih jenis kelamin.";
-
-    // 사진
-    // if (currentNumber == 3) title = "사진을 등록해 주세요";
-    // 자기소개
-    // if (currentNumber == 4) title = "자기소개를 작성해 주세요";
-  }
-
-  void showNextStep(ValueNotifier currentNumber, ValueNotifier isShowedNickname, ValueNotifier isShowedBirth, ValueNotifier isShowedGender) {
-    // 닉네임
-    if (currentNumber.value == 0) isShowedNickname.value = !isShowedNickname.value;
-    // 생년월일
-    if (currentNumber.value == 1) isShowedBirth.value = !isShowedBirth.value;
-    // 성별
-    if (currentNumber.value == 2) isShowedGender.value = !isShowedGender.value;
-    // if (currentNumber == 3) isOpacityPhoto = !isOpacityPhoto;
-    // if (currentNumber == 4) isOpacityIntroduce = !isOpacityIntroduce;
+  void processNextStep(ValueNotifier step) {
+    switch (step.value) {
+      case ProfileEditProcess.Name:
+        step.value = ProfileEditProcess.Birthday;
+        break;
+      case ProfileEditProcess.Birthday:
+        step.value = ProfileEditProcess.Gender;
+        break;
+      case ProfileEditProcess.Gender:
+        step.value = ProfileEditProcess.Gender;
+        break;
+    }
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    var currentNumber = useState<int>(0);
-    var title = useState<String>("name");
-    var guideText = useState<String>("Dengan nama yang diatur di profil KIFFY, Tidak dapat diubah setelah ini.");
-    var isShowedNickname = useState(false);
-    var isShowedBirth = useState(false);
-    var isShowedGender = useState(false);
-    var isShowedIntroduce = useState(false);
-    var isShowedPhoto = useState(false);
+    var process = useState(ProfileEditProcess.Name);
+    var processContent = useState<ProfileEditProcessContent>(ProfileEditProcessContent.of(process.value));
+
+    useValueChanged(process.value, (_, __) {
+      processContent.value = ProfileEditProcessContent.of(process.value);
+      return process.value;
+    });
 
     return Scaffold(
       body: SafeArea(
@@ -70,14 +79,14 @@ class SettingProfileScreen extends HookConsumerWidget {
                   children: [
                     Center(
                       child: Text(
-                        title.value,
+                        processContent.value.title,
                         style: const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
                       ),
                     ),
                     const SizedBox(height: 16),
                     Center(
                       child: Text(
-                        guideText.value,
+                        processContent.value.guideText,
                         textAlign: TextAlign.center,
                       ),
                     ),
@@ -98,10 +107,13 @@ class SettingProfileScreen extends HookConsumerWidget {
                         // SelectedPhotos(isOpacity: isOpacityPhoto),
 
                         // 성별 선택 :: 테스트
-                        SelectedGender(isShowed: isShowedGender.value),
+                        SelectedGender(isShowed: process.value == ProfileEditProcess.Gender),
 
                         // 생년월일
-                        AddBirthTextForm(hinText: "Birth day", labelText: "Birth day", isShowed: isShowedBirth.value),
+                        AddBirthTextForm(
+                            hinText: "Birth day",
+                            labelText: "Birth day",
+                            isShowed: [ProfileEditProcess.Gender, ProfileEditProcess.Birthday].contains(process.value)),
 
                         // 닉네임 기본으로 보여줌
                         const SettingNickName(hinText: "NickName", labelText: "NickName"),
@@ -112,9 +124,7 @@ class SettingProfileScreen extends HookConsumerWidget {
               ),
               InkWell(
                 onTap: () {
-                  currentNumber.value++;
-                  showNextStep(currentNumber, isShowedNickname, isShowedBirth, isShowedGender);
-                  upDateGuideText(currentNumber, title, guideText);
+                  processNextStep(process);
                 },
                 child: const ConfirmButton(text: "확인"),
               ),
@@ -125,76 +135,76 @@ class SettingProfileScreen extends HookConsumerWidget {
     );
   }
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     body: SafeArea(
-  //       child: Padding(
-  //         padding: const EdgeInsets.symmetric(horizontal: 51, vertical: 20),
-  //         child: Column(
-  //           children: [
-  //             Container(
-  //               decoration: BoxDecoration(border: Border.all(color: Colors.red)),
-  //               height: 100,
-  //               child: Column(
-  //                 mainAxisAlignment: MainAxisAlignment.center,
-  //                 children: [
-  //                   Center(
-  //                     child: Text(
-  //                       title,
-  //                       style: const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-  //                     ),
-  //                   ),
-  //                   const SizedBox(height: 16),
-  //                   Center(
-  //                     child: Text(
-  //                       guideText,
-  //                       textAlign: TextAlign.center,
-  //                     ),
-  //                   ),
-  //                 ],
-  //               ),
-  //             ),
-  //             const SizedBox(height: 70),
-  //             Form(
-  //               child: Expanded(
-  //                 child: Container(
-  //                   decoration: BoxDecoration(border: Border.all(color: Colors.red)),
-  //                   child: Column(
-  //                     children: [
-  //                       // // 자기소개
-  //                       // IntroduceTextForm(isOpacity: isOpacityIntroduce),
+// @override
+// Widget build(BuildContext context) {
+//   return Scaffold(
+//     body: SafeArea(
+//       child: Padding(
+//         padding: const EdgeInsets.symmetric(horizontal: 51, vertical: 20),
+//         child: Column(
+//           children: [
+//             Container(
+//               decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+//               height: 100,
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   Center(
+//                     child: Text(
+//                       title,
+//                       style: const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+//                     ),
+//                   ),
+//                   const SizedBox(height: 16),
+//                   Center(
+//                     child: Text(
+//                       guideText,
+//                       textAlign: TextAlign.center,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             const SizedBox(height: 70),
+//             Form(
+//               child: Expanded(
+//                 child: Container(
+//                   decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+//                   child: Column(
+//                     children: [
+//                       // // 자기소개
+//                       // IntroduceTextForm(isOpacity: isOpacityIntroduce),
 
-  //                       // // 사진 선택
-  //                       // SelectedPhotos(isOpacity: isOpacityPhoto),
+//                       // // 사진 선택
+//                       // SelectedPhotos(isOpacity: isOpacityPhoto),
 
-  //                       // 성별 선택 :: 테스트
-  //                       SelectedGender(isShowed: isShowedGender),
+//                       // 성별 선택 :: 테스트
+//                       SelectedGender(isShowed: isShowedGender),
 
-  //                       // 생년월일
-  //                       AddBirthTextForm(hinText: "Birth day", labelText: "Birth day", isShowed: isShowedBirth),
+//                       // 생년월일
+//                       AddBirthTextForm(hinText: "Birth day", labelText: "Birth day", isShowed: isShowedBirth),
 
-  //                       // 닉네임 기본으로 보여줌
-  //                       const SettingNickName(hinText: "NickName", labelText: "NickName"),
-  //                     ],
-  //                   ),
-  //                 ),
-  //               ),
-  //             ),
-  //             InkWell(
-  //               onTap: () {
-  //                 setState(() {
-  //                   currentNumber++;
-  //                   showNextStep();
-  //                   upDateGuideText();
-  //                 });
-  //               },
-  //               child: const ConfirmButton(text: "확인"),
-  //             ),
-  //           ],
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
+//                       // 닉네임 기본으로 보여줌
+//                       const SettingNickName(hinText: "NickName", labelText: "NickName"),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             InkWell(
+//               onTap: () {
+//                 setState(() {
+//                   currentNumber++;
+//                   showNextStep();
+//                   upDateGuideText();
+//                 });
+//               },
+//               child: const ConfirmButton(text: "확인"),
+//             ),
+//           ],
+//         ),
+//       ),
+//     ),
+//   );
+// }
 }
