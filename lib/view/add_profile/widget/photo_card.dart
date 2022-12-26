d84c36f86a6a8650258b27c5451f9d060c4d4434
Munbear <pdwssf@gmail.com>
2022-12-26 21:38:01 +0900

    사진 파일 받아오기

--- lib/view/add_profile/widget/photo_card.dart
+++ lib/view/add_profile/widget/photo_card.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -8,21 +9,19 @@ import 'package:flutter_svg/flutter_svg.dart';
 class PhotoCard extends HookConsumerWidget {
   PhotoCard({super.key});
 
-  File? _image;
   final picker = ImagePicker();
 
-  Future addImage(ImageSource imageSource) async {
+  Future addImage(ValueNotifier<File?> _image, ImageSource imageSource) async {
     final image = await picker.pickImage(source: imageSource);
-
-    // setState(() {
-    //   _image = File(image!.path);
-    // });
+    _image.value = File(image!.path);
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final image = useState<File?>(null);
+
     return InkWell(
-      onTap: () => addImage(ImageSource.gallery),
+      onTap: () => addImage(image, ImageSource.gallery),
       child: Container(
         margin: const EdgeInsets.fromLTRB(7.5, 13, 7.5, 13),
         width: 109,
@@ -31,11 +30,11 @@ class PhotoCard extends HookConsumerWidget {
           border: Border.all(color: const Color(0xff0031AA), width: 1),
           borderRadius: const BorderRadius.all(Radius.circular(7)),
         ),
-        child: _image == null
+        child: image.value == null
             ? Center(child: SvgPicture.asset("assets/svg/camera.svg"))
             : ClipRRect(
                 borderRadius: BorderRadius.circular(7),
-                child: Image.file(File(_image!.path), fit: BoxFit.fill),
+                child: Image.file(File(image.value!.path), fit: BoxFit.fill),
               ),
       ),
     );
