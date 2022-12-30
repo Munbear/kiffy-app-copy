67fd4c8363d0b426439f6b89928a1338dd9bf5cf
Munbear <pdwssf@gmail.com>
2022-12-30 17:33:19 +0900

    이미지 삭제 완료

--- lib/view/add_profile/widget/photo_card.dart
+++ lib/view/add_profile/widget/photo_card.dart
@@ -9,46 +9,63 @@ import 'package:flutter_svg/flutter_svg.dart';
 class PhotoCard extends HookConsumerWidget {
   PhotoCard({super.key});
 
-  final picker = ImagePicker();
-  late String saveFile;
-  late String imageName;
-
-  Future addImage(ValueNotifier<File?> _image, ImageSource imageSource) async {
+  // 이미지 불러오기
+  Future addImage(ValueNotifier<File?> imagePath, ValueNotifier<File?> imageFile, ImageSource imageSource) async {
+    final picker = ImagePicker();
     final file = await picker.pickImage(source: imageSource);
-    // null 체크
-    if (file == null) {
-      return;
+    if (file != null) {
+      imagePath.value = File(file.path);
+      imageFile.value = File(file.name);
     } else {
-      _image.value = File(file.path);
-      // 파일 이름 가지고 옴
-      imageName = File(file.name).toString();
-      print(imageName);
-      // 파일 저장됨
-      // File(file.path).copySync('/Users/munbeaer/Downloads/24/$imageName').toString();
+      return;
+    }
+  }
+
+  // 이미지 삭제
+  Future deleteImage(ValueNotifier<File?> imagePath, ValueNotifier<File?> imageFile) async {
+    if (imagePath.value != null) {
+      imagePath.value = null;
+      imageFile.value = null;
     }
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final image = useState<File?>(null);
+    final imagePath = useState<File?>(null);
+    final imageFile = useState<File?>(null);
 
-    return InkWell(
-      onTap: () => addImage(image, ImageSource.gallery),
-      child: Container(
-        margin: const EdgeInsets.fromLTRB(7.5, 13, 7.5, 13),
-        width: 109,
-        height: 162,
-        decoration: BoxDecoration(
-          border: Border.all(color: const Color(0xff0031AA), width: 1),
-          borderRadius: const BorderRadius.all(Radius.circular(7)),
+    return Stack(
+      children: [
+        Container(
+          margin: const EdgeInsets.fromLTRB(7.5, 13, 7.5, 13),
+          width: 109,
+          height: 162,
+          decoration: BoxDecoration(
+            border: Border.all(color: const Color(0xff0031AA), width: 1),
+            borderRadius: const BorderRadius.all(Radius.circular(7)),
+          ),
+          child: imagePath.value == null
+              ? InkWell(
+                  onTap: () => addImage(imagePath, imageFile, ImageSource.gallery),
+                  child: Center(child: SvgPicture.asset("assets/svg/camera.svg")),
+                )
+              : ClipRRect(
+                  borderRadius: BorderRadius.circular(7),
+                  child: Image.file(File(imagePath.value!.path), fit: BoxFit.fill),
+                ),
         ),
-        child: image.value == null
-            ? Center(child: SvgPicture.asset("assets/svg/camera.svg"))
-            : ClipRRect(
-                borderRadius: BorderRadius.circular(7),
-                child: Image.file(File(image.value!.path), fit: BoxFit.fill),
-              ),
-      ),
+        Visibility(
+          visible: imageFile.value != null ? true : false,
+          child: Positioned(
+            right: -7,
+            top: -5,
+            child: GestureDetector(
+              onTap: () => deleteImage(imagePath, imageFile),
+              child: SvgPicture.asset("assets/svg/cancel_icon.svg"),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
