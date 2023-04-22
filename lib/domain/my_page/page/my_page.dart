e9cb9528038d72fc5fb6759a65846563498d680f
labyu <labyu2020@gmail.com>
2023-04-22 16:17:01 +0900

    프로필 쪽 나이 나타나도록 변경

--- lib/domain/my_page/page/my_page.dart
+++ lib/domain/my_page/page/my_page.dart
@@ -4,6 +4,7 @@ import 'package:Kiffy/domain/my_page/provider/user_profile_info.dart';
 import 'package:Kiffy/domain/my_page/widget/my_page_button.dart';
 import 'package:Kiffy/model/media_view/media_view.dart';
 import 'package:Kiffy/model/user_profile_view/user_profile_view.dart';
+import 'package:Kiffy/util/BirthDateUtil.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -81,37 +82,28 @@ class MyPage extends HookConsumerWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     if (profile.value != null)
-                      Text(
-                        profile.value!.name,
-                        style: const TextStyle(
-                          fontSize: 26,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.white,
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: profile.value!.name,
+                              style: const TextStyle(
+                                fontSize: 26,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white,
+                              ),
+                            ),
+                            TextSpan(
+                              text: " ${BirthDateUtil.getAge(BirthDateUtil.parseBirthDate(profile.value!.birthDate)).toString()}",
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white70,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        if (profile.value != null)
-                          Text(
-                            profile.value!.birthDate,
-                            style: const TextStyle(
-                              fontSize: 26,
-                              fontWeight: FontWeight.w700,
-                              color: Colors.white,
-                            ),
-                          ),
-                        // const SizedBox(width: 16),
-                        // Text(
-                        //   profile.value.,
-                        //   style: TextStyle(
-                        //     fontSize: 20,
-                        //     fontWeight: FontWeight.w400,
-                        //     color: Colors.white,
-                        //   ),
-                        // ),
-                      ],
-                    ),
                   ],
                 ),
               ),
