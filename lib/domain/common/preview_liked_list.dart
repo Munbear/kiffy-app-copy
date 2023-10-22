ab3a6861dce5822c8423ff4b27209827e4da5fee
labyu <labyu2020@gmail.com>
2023-10-22 19:03:19 +0900

    정리

--- lib/domain/common/preview_liked_list.dart
+++ lib/domain/common/preview_liked_list.dart
@@ -1,10 +1,11 @@
 import 'package:Kiffy/domain/explore/widget/explore_wished_list_item.dart';
+import 'package:Kiffy/infra/openapi_client.dart';
 import 'package:Kiffy/infra/wish_client.dart';
 import 'package:Kiffy/model/wish_other_profiles_view/wish_other_profiles_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:openapi/openapi.dart';
 
-import '../../model/user_profile_view/user_profile_view.dart';
 
 class PreviewLikedList extends ConsumerStatefulWidget {
   const PreviewLikedList({super.key});
@@ -15,17 +16,21 @@ class PreviewLikedList extends ConsumerStatefulWidget {
 }
 
 class _PreviewLikedListState extends ConsumerState<PreviewLikedList> {
+  List<UserProfileView> wishOtherProfiles = List.empty();
+
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      ref.read(wishClientProvider).getWishOthersProfiles();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      var response = await ref.read(openApiProvider).getWishApi().apiWishV1WishOtherGet();
+      setState(() {
+        wishOtherProfiles = response.data!.list!.toList();
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    final wishOtherProfiles = ref.watch(wishMeUsersProvider);
 
     return Stack(
       children: [
@@ -35,14 +40,13 @@ class _PreviewLikedListState extends ConsumerState<PreviewLikedList> {
             padding: const EdgeInsets.only(left: 20),
             scrollDirection: Axis.horizontal,
             children: [
-              if (wishOtherProfiles != null)
-                ...wishOtherProfiles.map(
-                  (wishOtherProfile) => ExploreWishedListItem(
-                    profile: wishOtherProfile,
-                  ),
+              ...wishOtherProfiles.map(
+                    (wishOtherProfile) => ExploreWishedListItem(
+                  profile: wishOtherProfile,
                 ),
+              ),
               // 자세히 보기
-              if (wishOtherProfiles != null && wishOtherProfiles.isEmpty)
+              if (wishOtherProfiles.isEmpty)
                 const Center(child: Text("아직 위시를 받은 적이 없습니다."))
               // 위시 받은 유저 리스트
               // wishOtherProfiles.list.isNotEmpty ? const ExploreWishedListMore() : const Center(child: Text("아직 위시를 받은 적이 없습니다.")),
@@ -50,7 +54,7 @@ class _PreviewLikedListState extends ConsumerState<PreviewLikedList> {
           ),
         ),
 
-        // 유저 성별에 따라사 보여지는 텍스트가 달라짐 :: 업데이트
+        // TODO 유저 성별에 따라사 보여지는 텍스트가 달라짐 :: 업데이트
         // const LockBulr(mainText: "It's still being implemented.", subText: "It's still being implemented.")
       ],
     );
--- lib/infra/user_info.dart
+++
@@ -1,3 +0,0 @@
-import 'package:Kiffy/model/user_profile_view/user_profile_view.dart';
-
-// final userInfo = StateProvider<UserProfileView?>((ref) => null);
