3c7c261d08e00ef664f504598f6d84b879463b60
labyu <labyu2020@gmail.com>
2023-10-30 21:53:40 +0900

    나에게 위시한 사용자 기간 남은 것만 볼 수 있도록 변경

--- lib/screen_module/common/widget/other_wish_user/other_wish_preview_section.dart
+++ lib/screen_module/common/widget/other_wish_user/other_wish_preview_section.dart
@@ -1,9 +1,11 @@
 import 'package:Kiffy/screen_module/common/widget/other_wish_user/other_wish_preview_chips.dart';
 import 'package:Kiffy/screen_module/explore/provider/explore_users_provider.dart';
 import 'package:Kiffy/screen_module/wish/provider/other_wish_users_reader.dart';
+import 'package:Kiffy/screen_module/wish/provider/wish_remain_calculator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:openapi/openapi.dart';
 
 class OtherWishPreviewSection extends ConsumerStatefulWidget {
   const OtherWishPreviewSection({super.key});
@@ -23,12 +25,22 @@ class _OtherWishPreviewSectionState
       return const SizedBox(height: 95, child: OtherWishPreviewChipsSkeleton());
     }
 
+    final List<OtherWishUserProfileView> remainedOtherWishUsers =
+        otherWishState.requireValue.otherWishes.isNotEmpty
+            ? otherWishState.requireValue.otherWishes
+                .takeWhile((otherWish) => ref
+                    .read(wishRemainCalculatorProvider)
+                    .calculate(otherWish.createdAt)
+                    .isRemained())
+                .take(10)
+                .toList()
+            : [];
+
     return otherWishState.requireValue.otherWishes.isNotEmpty
         ? SizedBox(
             height: 95,
             child: OtherWishPreviewChips(
-              otherWishes:
-                  otherWishState.requireValue.otherWishes.take(10).toList(),
+              otherWishes: remainedOtherWishUsers,
               onTap: (wishId) {
                 context.push("/other-wish/wish/${wishId}/detail").then((value) {
                   ref.read(otherWishUsersReaderProvider.notifier).refresh();
