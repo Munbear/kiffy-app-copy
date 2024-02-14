35845487d7bef5f56524d466e95c2157527a1ca2
Munbear <pdwssf@gmail.com>
2024-02-14 22:12:55 +0900

    feed 작성 사진 ui 레이아웃

--- lib/screen/create_feed/create_feed_screen.dart
+++ lib/screen/create_feed/create_feed_screen.dart
@@ -54,47 +54,76 @@ class CreateFeedScreen extends StatelessWidget {
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: Column(
-                  children: [
-                    // 유저 정보
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 32,
-                          height: 32,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(50),
-                            child: Image.asset(
-                              "assets/images/test_image.png",
-                              fit: BoxFit.cover,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      // 유저 정보
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 32,
+                            height: 32,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(50),
+                              child: Image.asset(
+                                "assets/images/test_image.png",
+                                fit: BoxFit.cover,
+                              ),
                             ),
                           ),
-                        ),
-                        Gab.width12,
-                        Text(
-                          "userName",
-                          style: textStyle.bodyMedium,
-                        )
-                      ],
-                    ),
-                    Gab.height12,
-                    const Divider(),
-                    // 텍스트 영역
-                    TextFormField(
-                      inputFormatters: [
-                        LengthLimitingTextInputFormatter(500),
-                      ],
-                      maxLines: null,
-                      decoration: const InputDecoration(
-                        contentPadding: EdgeInsets.zero,
-                        border: InputBorder.none,
-                        hintText: "내용을 입력해 주세요",
+                          Gab.width12,
+                          Text(
+                            "userName",
+                            style: textStyle.bodyMedium,
+                          )
+                        ],
                       ),
-                    ),
-                  ],
+                      Gab.height12,
+                      const Divider(),
+                      // 텍스트 영역
+                      TextFormField(
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(500),
+                        ],
+                        maxLines: null,
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.zero,
+                          border: InputBorder.none,
+                          hintText: "내용을 입력해 주세요",
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
+            SizedBox(
+              height: 128,
+              child: ListView.separated(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                separatorBuilder: (context, index) {
+                  return Gab.width4;
+                },
+                itemBuilder: (context, index) {
+                  return Stack(
+                    children: [
+                      SizedBox(
+                        width: 112,
+                        height: 112,
+                        child: Image.asset(
+                          "assets/images/test_image.png",
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                    ],
+                  );
+                },
+              ),
+            ),
+            // 사진 불러오기 영역
             Container(
               color: Colors.grey[200],
               padding: const EdgeInsets.only(right: 20),
