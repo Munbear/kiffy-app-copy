7829f27156d5196df7f258fb825756333ac11398
Munbear <pdwssf@gmail.com>
2024-04-20 22:33:50 +0900

    피드 매칭 취소 프리뷰 업데이트

--- lib/screen_module/match/widget/feed_option_bottom_sheet_container.dart
+++ lib/screen_module/match/widget/feed_option_bottom_sheet_container.dart
@@ -1,4 +1,5 @@
 import 'package:Kiffy/screen_module/match/provider/community_provider.dart';
+import 'package:Kiffy/screen_module/match/provider/matched_user_provider.dart';
 import 'package:Kiffy/screen_module/my/provider/my_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -39,8 +40,11 @@ class FeedOptionBottomSheetContainer extends ConsumerWidget {
                         .read(communityProvider.notifier)
                         .cancelMatching(authorId)
                         .then(
-                          (value) => context.pop(),
-                        );
+                        (value) {
+                          context.pop();
+                          ref.invalidate(matchedUsersProvider);
+                        },
+                      );
               },
               child: Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16),
